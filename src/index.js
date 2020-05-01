100a7868090cde2742bccb93b655ef7596c28d80
omnisci3nce <17525998+omnisci3nce@users.noreply.github.com>
2020-05-01 10:46:32 +1000

    use spec to update state

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import { map } from 'ramda'
+// import { map } from 'ramda'
+import { prop, spec } from './utils'
 import Idle1 from './img/idle-00.png'
 import Idle2 from './img/idle-01.png'
 import Idle3 from './img/idle-02.png'
@@ -24,24 +25,40 @@ let idleAnimFrames = [
     loadImg(Idle4),
 ]
 
-
-// game state
 let state = {
 	playerPos: { x:0, y:0 },
     frame: 0,
     animFrame: 0
 }
 
+// test spec
+
+let counters = {
+    x: 0
+}
+const addOne = c => {
+    return c.x + 1
+}
+
+const updatePlayerPos = state => {
+    return {x:state.playerPos.x, y:state.playerPos.y}
+}
+
 const next = () => {
     state.animFrame = Math.trunc(state.frame/2)
-    // console.log('frame: ', state.frame)
-    // console.log('animFrame: ', state.animFrame)
 
     state.frame += 1
 
     if (state.frame / 2 >= 4) {
         state.frame = 0
     }
+
+    state = spec({
+        playerPos: updatePlayerPos,
+        frame: prop('frame'),
+        animFrame: prop('animFrame')
+    })(state)
+    // console.log(state)
 }
 
 /* 10 frames per second
@@ -58,25 +75,51 @@ const draw = () => {
 	ctx.fillStyle = '#EDF2F7'
     ctx.fillRect(0, 0, canvas.width, canvas.height)
     // console.log(state)
-    ctx.drawImage(idleAnimFrames[state.animFrame], 0, 0, 50, 30, 0, 0, 50 * SCALE , 30 * SCALE )
+    ctx.drawImage(idleAnimFrames[state.animFrame], 0, 0, 50, 30, state.playerPos.x, state.playerPos.y, 50 * SCALE , 30 * SCALE )
 }
 let globalFrame = 0
+
+// mutable state
+const inputQueue = []
+
+const handleInput = ( q ) => {
+    q.shift()
+}
+
 const step = t1 => t2 => {
-	if (t2 - t1 > 100) {
-        // console.log('global frame: ', globalFrame)
-        globalFrame += 1
-        // if (globalFrame >= 16) {
-        //     return
-        // }
-		next()
+	if (t2 - t1 > 1000) {
+        // globalFrame += 1
+
+        // handle inputs 
+
+
+        // update state based on state and inputs
+        next()
+        console.log(inputQueue)
+        handleInput(inputQueue)
+
+
+        // draw to screen
 		draw()
-		// console.log('Draw Complete.')
+		
 		window.requestAnimationFrame(step(t2))
 	} else {
 		window.requestAnimationFrame(step(t1))
 	}
 }
 
+// Key events
+
+window.addEventListener('keydown', e => {
+    switch (e.key) {
+        case 'w': case 'ArrowUp':
+            inputQueue.push('up')
+            break
+        case 'a': case 'ArrowLeft': console.log('left pressed'); break
+        case 's': case 'ArrowDown': console.log('down pressed'); break
+        case 'd': case 'ArrowRight': console.log('right pressed'); break
+    }
+})
 
 
 window.onload = () => {
